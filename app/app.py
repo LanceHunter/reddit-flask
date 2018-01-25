fdefce96c17b24b27e17b8fab30d5fa55466d5e6
Paige Throckmorton <path4942@gmail.com>
2018-01-25 11:56:50 -0600

    Commiting before pull with master

--- app/app.py
+++ app/app.py
@@ -1,6 +1,14 @@
-from flask import flask
-app = Flask(__app__)
+import pgdb
+from flask import Flask, request, send_from_directory
+app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello'
+# @app.route('/')
+# def hello_world():
+#     return send_from_directory('app/public', 'index.html')
+
+if __name__ == '__main__':
+    app.run()
+
+@app.route('/posts', methods=['GET'])
+def get_posts():
+    return jsonify({'posts': posts})
