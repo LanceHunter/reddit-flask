30fe44fe316143b6f1e5d72c76b7e1c56148a6cd
Lance Hunter <lancehunter@gmail.com>
2018-01-25 14:58:45 -0600

    Updating lance branch

--- tom.py
+++ tom.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, send_from_directory
 app = Flask(__name__)
 
+hostname = 'localhost'
+database = 'reddit-clone'
+
 @app.route('/')
 def hello_world():
     return send_from_directory('app/public', 'index.html')
+
+@
 
 if __name__ == '__main__':
     app.run()
