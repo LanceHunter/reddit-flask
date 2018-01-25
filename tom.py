956f39bfcd47cdebd45a30dcc2572de5cca1a9e7
Paige Throckmorton <path4942@gmail.com>
2018-01-25 14:34:14 -0600

    WTF

--- tom.py
+++ tom.py
@@ -1,9 +1,12 @@
+from pg import DB
+db = DB()
 from flask import Flask, request, send_from_directory
 app = Flask(__name__)
+
+
+if __name__ == '__main__':
+    app.run()
 
 @app.route('/')
 def hello_world():
     return send_from_directory('app/public', 'index.html')
-
-if __name__ == '__main__':
-    app.run()
