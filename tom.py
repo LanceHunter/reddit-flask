4375ef9de67acb9e02012b4cc27147191e610f2a
Thomas Palzkill <tpalzkilll@gmail.com>
2018-01-25 14:55:49 -0600

    Tom to the master

--- tom.py
+++ tom.py
@@ -1,9 +1,20 @@
-from flask import Flask, request, send_from_directory
+from pg import DB
+from flask import Flask, request, send_from_directory, jsonify
 app = Flask(__name__)
+db = DB(dbname = 'reddit-clone', host='localhost')
 
 @app.route('/')
 def hello_world():
     return send_from_directory('app/public', 'index.html')
+
+@app.route('/api/posts')
+def get_posts():
+    firstResults = db.query('select * from posts')
+    results = firstResults.dictresult()
+
+    commentResults = db.query('select * from "comments" where "post_id" in (posts.map(p => p.id))')
+    return jsonify(results)
+
 
 if __name__ == '__main__':
     app.run()
